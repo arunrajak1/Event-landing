6126efe366fbcf828c863b119480071149eec8f7
Arun Rajak <arunrajak527@gmail.com>
2024-05-20 16:08:44 +0530

    Update VerticleLine.jsx
    remove scrollbar

--- src/Components/VerticleLine.jsx
+++ src/Components/VerticleLine.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from 'react-typed';
 
 const VerticleLine = () => {
   return (
-    <div className="flex justify-center items-center  bg-violet-300 h-screen">
+    <div className="flex justify-center items-center  bg-violet-300 h-screen  overflow-hidden">
       <div className="relative w-10 h-4/5">
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-full rotate-90 whitespace-nowrap text-white animate-scroll">
         
